ab3eb1f8988e13bd6acdb98ddd6d4c4949d3370a
AnaCarla <ananana745@gmail.com>
2012-08-26 23:04:53 -0300

    Adicionando bin e src

---
+++ src/test/git/input/RecebeTeste.java
@@ -0,0 +1,16 @@
+package test.git.input;
+
+public class RecebeTeste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Teste test = new Teste(12, "Não sei",1);
+		test.ImprimiTeste();
+
+	}
+
+}
---
+++ src/test/git/input/Teste.java
@@ -0,0 +1,33 @@
+package test.git.input;
+/**
+
+ * @author Ana Carla
+ * 
+ * @Verifica respostas e notas dos alunos
+ */
+public class Teste {
+	
+	private String resposta;
+	private float nota;
+	private int codigo_aluno;
+	
+		public Teste(int codigo_aluno, String resposta, float nota){
+		
+			this.resposta=resposta;
+			this.codigo_aluno= codigo_aluno;
+			this.nota= nota;
+			
+		}
+		public void ImprimiTeste(){
+			
+			System.out.println("Teste 1\n");
+			System.out.println("Aluno: " + codigo_aluno);
+			System.out.println("Pergunta 1: "+ resposta);
+			System.out.println("Nota: " + nota);
+							
+		}
+		
+		
+		
+
+}
